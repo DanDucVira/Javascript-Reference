1bf4da626c23f369d59d36b7cc0d418de524b4d3
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2019-09-04 13:33:47 -0400

    added file syntax.js

---
+++ JavascriptSyntax/syntax.js
@@ -0,0 +1,12 @@
+//This is how you write a comment in javascript(the compiler doesnt output this)
+
+
+//This is a variable( a box of data)
+var car = "Telsa"
+console.log(car);
+
+//this is how you use numbers
+var number = "29";
+console.log(number);
+
+
