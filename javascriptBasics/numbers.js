a497d6be53b8e2b40765d29a9b6685acb5e4fd35
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2019-09-08 13:58:08 -0400

    finished numbers.js

---
+++ javascriptBasics/numbers.js
@@ -0,0 +1,23 @@
+//numbers
+
+let number = 10;
+let pi = 3.14;
+
+console.log(number);
+console.log(pi);
+
+//math operators +. -, *, /,**,%
+console.log(10 + 2);
+console.log(10 - 2);
+console.log(10 * 2);
+console.log(10 / 2);
+console.log(10 ** 2);
+console.log(10 % 2);
+
+let add = 10;
+add++
+console.log(add);
+
+let minis = 10;
+minis--;
+console.log(minis--);
