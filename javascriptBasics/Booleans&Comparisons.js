332b7092b0bc861e94dd780f56a71b6075cd54f0
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2019-09-16 13:09:11 -0400

    finished booleans & comparison

---
+++ javascriptBasics/Booleans&Comparisons.js
@@ -0,0 +1,25 @@
+//True and False
+
+//methods can return booleans
+
+let email = "dducasviramontes@gmail.com";
+
+let result = email.includes('@');
+console.log(result);
+//output  = true
+
+
+//comparison operators
+let age = 29;
+
+console.log(age == 29); // equal to
+console.log(age != 29); //not equal
+console.log(age > 29); //greater then
+console.log(age < 29); // less then
+console.log(age <= 29); // less then or equal
+console.log(age >= 29); // greater then or equal
+
+
+let name = 'daniel';
+
+console.log(name == 'daniel');
