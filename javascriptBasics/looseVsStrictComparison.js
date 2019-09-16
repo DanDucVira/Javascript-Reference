bb07342dc5b5761484e85128dc30d6296fdaf40f
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2019-09-16 13:28:14 -0400

    finished loose vs strict comparison

---
+++ javascriptBasics/looseVsStrictComparison.js
@@ -0,0 +1,21 @@
+let age = 29;
+
+//loose comparison(different types can still be equal)
+
+console.log(age == 29);
+console.log(age == '29');
+
+console.log(age != 29);
+console.log(age != '29');
+
+
+
+
+
+//strict comparison(different types cannot be equal)
+console.log(age === 29);
+console.log(age === '29');
+
+
+console.log(age !== 29);
+console.log(age !== '29');
