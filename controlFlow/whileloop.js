3c8b1a9142bcb2674398432709ad78f2233103ad
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2019-09-17 13:03:35 -0400

    finished whileloop.js

---
+++ controlFlow/whileloop.js
@@ -0,0 +1,15 @@
+//while loops
+// let i = 0; //declare the variable outside of the while loop
+// while (i < 5) {
+//     console.log('in loop', i);
+//     i++;
+// }
+// console.log('loop finished');
+
+let names = ['daniel', 'sarah', 'jimmy'];
+let i = 0;
+while (i < 5) {
+    console.log(names[i]);
+    i++;
+}
+console.log('loop finished');
