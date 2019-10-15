4795176226c1151c8ae2c87efd250f2bc1cae847
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2019-10-14 20:08:43 -0400

    finished functions vs methods

--- functions_methods/functions_methods.js
+++ functions_methods/functions_methods.js
@@ -1,5 +1,10 @@
 const name = "Daniel";
 
 //functions
+const greet = () => 'hello';
+let resultOne = greet();
+console.log(resultOne);
 
 //methods
+let result2 = name.toUpperCase();
+console.log(result2);
