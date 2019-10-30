0672d88946d5cfd90083946274429d2f4b24f6be
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2019-10-30 13:46:24 -0400

    finished creating object literals lecture

---
+++ objectLiterals/objectLiterals.js
@@ -0,0 +1,16 @@
+//object literals
+
+
+
+//creating object literals
+let user = {
+    name: "Daniel",
+    age: 29,
+    email: "daniel@gmail.com",
+    location: "Canada",
+    blogs: ["why tacos rule", "10 things to make with hot sauce"]
+};
+
+console.log(user);
+console.log(user.name);
+console.log(user["name"]);
