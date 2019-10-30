2f19d01eb77e9cea719f7b0e3d589dcd71087dd8
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2019-10-30 13:57:05 -0400

    finished adding methods lecture

--- objectLiterals/objectLiterals.js
+++ objectLiterals/objectLiterals.js
@@ -8,9 +8,20 @@ let user = {
     age: 29,
     email: "daniel@gmail.com",
     location: "Canada",
-    blogs: ["why tacos rule", "10 things to make with hot sauce"]
+    blogs: ["why tacos rule", "10 things to make with hot sauce"],
+    login: function () {
+        console.log("the user logged in");
+    },
+    logout: function () {
+        console.log("user logged out");
+    }
 };
 
 console.log(user);
 console.log(user.name);
 console.log(user["name"]);
+
+
+//addeing methods
+user.login();
+user.logout();
