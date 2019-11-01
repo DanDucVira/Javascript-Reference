badd12a06b81d39f52935855c0170d3e36e4a49a
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2019-11-01 13:13:01 -0400

    finished lecture on other ways to query the dom

--- Dom/dom.js
+++ Dom/dom.js
@@ -15,3 +15,14 @@ console.log(all);
 all.forEach(all => {
     console.log(all);
 });
+
+//get an element by ID
+const title = document.getElementById('page-title');
+console.log(title)
+
+//get elements by their class name
+const error2 = document.getElementsByClassName('error');
+console.log(error2);
+//get elements by their tag name
+const pageTitle = document.getElementsByTagName('h1');
+console.log(pageTitle);
