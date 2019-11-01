2f9d369a93eb99c6281058f3c5ff7fe09c8a7048
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2019-11-01 12:51:40 -0400

    finished query selector lecture

---
+++ Dom/dom.js
@@ -0,0 +1,17 @@
+//The Query Selector
+
+const para = document.querySelector('p');
+console.log(para);
+
+const error = document.querySelector('.error');
+
+console.log(error);
+
+
+
+const all = document.querySelectorAll('p');
+console.log(all);
+
+all.forEach(all => {
+    console.log(all);
+});
