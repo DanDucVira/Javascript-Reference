ea14b803bda6ff947cd30d3d0dcbd4db9e42912e
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2019-11-03 12:29:32 -0500

    started lecture on adding and changing page content

--- Dom/dom.js
+++ Dom/dom.js
@@ -26,3 +26,18 @@ console.log(error2);
 //get elements by their tag name
 const pageTitle = document.getElementsByTagName('h1');
 console.log(pageTitle);
+
+
+
+//adding & changing page content
+
+const changPara = document.querySelector('p');
+// console.log(changPara.innerText);
+// changPara.innerText = "GoodBye World";
+// console.log(changPara);
+
+const changParas = document.querySelectorAll('p');
+
+changParas.forEach(changPara => {
+    console.log(changPara.innerText);
+});
