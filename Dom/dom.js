e07eac1eb644d62be9413f7554ac9ae93be158b1
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2019-11-05 13:20:39 -0500

    finished lecture on getting & setting Attributes

--- Dom/dom.js
+++ Dom/dom.js
@@ -55,3 +55,18 @@ const people = ['Daniel', 'Dragonborn', 'skyrim'];
 people.forEach(person => {
     content.innerHTML += `<p>${person}</p>`;
 });
+
+
+//Getting & setting Attributes
+const link = document.querySelector('a');
+
+console.log(link.getAttribute('href'));
+
+link.setAttribute('href', 'htpps://www.thenetninja.co.uk');
+
+link.innerHTML = 'The Net Ninja Website';
+
+const mssg = document.querySelector('p');
+console.log(mssg.getAttribute('class'));
+mssg.setAttribute('class', 'success');
+mssg.setAttribute('style', 'color:green;');
