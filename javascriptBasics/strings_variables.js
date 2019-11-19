0f91580b8f9a6f1cf46f5810ee66149e28970b26
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2019-11-19 14:21:32 -0500

    finished lecture on strings and variables

---
+++ javascriptBasics/strings_variables.js
@@ -0,0 +1,15 @@
+let name = "Daniel Ducas-Viramontes";
+
+let firstName = "Sarah";
+let lastName = "Lamontagne";
+let FullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+console.log(name);
+
+//challenge area
+
+let city = "ottawa";
+let country = "Canada";
+
+let location = city + "," + country;
