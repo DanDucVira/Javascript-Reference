460cfdb1fa5e28a8d5a697160311cd1ada102838
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2019-11-22 14:05:21 -0500

    finished numbers lecture

---
+++ javascriptBasics/numbers.js
@@ -0,0 +1,19 @@
+//Numbers in js
+//JavaScript has One number type
+//Positive numbers!
+//negative numbers!
+//Whole number(integers)!
+//Decimal numbers!
+
+let age = 29
+let futurAge;
+fiveYears = 5;
+
+let futurAge = age + fiveYears;
+console.log(futurAge);
+
+//Nan --- not a Number
+//NaN is a numeric value that represents something that is not...a number\
+/8/example
+0 / 0 //Nan
+1 + NaN; //Nan
