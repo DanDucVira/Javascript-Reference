a8c45aa828a93b40191f015e20696ca79b3b8006
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2019-12-03 13:54:13 -0500

    added more info on math.random

---
+++ javascriptBasics/mathObjectRandomNumbers.js
@@ -0,0 +1,20 @@
+// Math Object
+
+/*
+Contains properties and methods for mathematical constants and functions
+*/
+
+Math.pi //3.141592653589793
+
+//Math.random() gives us a random decimal between 0 and 1(non-inclusive)
+//Rounding a number:
+Math.round(4.9) //5
+
+//Absolute value:
+Math.abs(-456) //456
+
+//Raises 2 to the 5 power
+Math.pow(2.5) //32
+
+//Removes decimal:
+Math.floor(3.9999) //3
