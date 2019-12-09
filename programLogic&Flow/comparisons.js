3b88e484d37c5c57ee8baf383516e6944d057927
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2019-12-09 12:53:19 -0500

    added comparisons examples

---
+++ programLogic&Flow/comparisons.js
@@ -0,0 +1,25 @@
+// > greater than
+// < less than
+// >= greater than or equal to
+// <= less than or equal to
+// == equality
+// != not equal
+// === struct equality
+// !== strict non-equality
+
+
+//some examples
+//Notice these all return a boolean!
+/*
+Through it's uncommon, you can compare strings. just be carful, things get dicey when dealing with case, special characters, and accents!
+
+*/
+10 > 1;  ///true
+0.2 > 0.3; //false
+-10 < 0; //true
+50.5 < 5; //false
+0.5 <= 0.5; //true
+99 >= 4; //true
+99 >= 99 //true
+'a' < 'b'; //true
+'A' > 'a'; //false
