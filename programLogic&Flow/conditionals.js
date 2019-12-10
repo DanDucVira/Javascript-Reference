c5c036433a366911d74f1293c8be883ef1a0f340
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2019-12-10 13:42:16 -0500

    added an example of conditionals

--- programLogic&Flow/conditionals.js
+++ programLogic&Flow/conditionals.js
@@ -22,15 +22,24 @@ if (rating === 3) {
 //ELSE
 
 let rating = -99;
-if(rating === 3){
+if (rating === 3) {
     console.log("YOU ARE A SUPERSTAR!");
-}
-else if(rating === 2){
+} else if (rating === 2) {
     console.log("MEETS EXPECTATIONS");
-}
-else if(rating === 1){
+} else if (rating === 1) {
     console.log("NEEDS IMPORVEMENT");
-}
-else{
+} else {
     console.log("INVALID RATING");
+}
+
+//Example 
+
+let hightScore = 1430;
+let userScore = 1200;
+
+if (userScore >= highScore) {
+    console.log(`Congrats, you have the new high score of ${userScore}`);
+    hightScore = userScore;
+} else {
+    console.log(`Good Game, Your Score of ${userScore} did not beat the high score of ${highScore}`);
 }
