cd79819e5ed19653a818ec2a6cc176db22cabd84
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2019-12-10 13:37:05 -0500

    added more conditionals examples

--- programLogic&Flow/conditionals.js
+++ programLogic&Flow/conditionals.js
@@ -2,9 +2,35 @@
 //Making decisions with code
 
 //Example 1
+
+//IF STATEMENT
 let rating = 3;
 
 if (rating === 3) {
     console.log('YOU ARE A SUPERSTAR!');
 }
 
+//ELSE IF
+let rating = 2;
+if (rating === 3) {
+    console.log('YOU ARE A SUPERSTAR!');
+} else if (rating === 2) {
+    console.log("MEETS EXPECTATIONS");
+}
+
+
+//ELSE
+
+let rating = -99;
+if(rating === 3){
+    console.log("YOU ARE A SUPERSTAR!");
+}
+else if(rating === 2){
+    console.log("MEETS EXPECTATIONS");
+}
+else if(rating === 1){
+    console.log("NEEDS IMPORVEMENT");
+}
+else{
+    console.log("INVALID RATING");
+}
