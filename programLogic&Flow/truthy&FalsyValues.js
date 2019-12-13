63c19a51760866969dd82695ac0e19e7aca6172b
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2019-12-13 13:44:03 -0500

    added examples of true and false values

---
+++ programLogic&Flow/truthy&FalsyValues.js
@@ -0,0 +1,14 @@
+// All values have an inherent truthy or falsy boolean value
+/*
+Falsy values:
+-false
+-0
+-""(empty string)
+-null
+-undefined
+-NaN
+
+----Everything else is truthy!----
+
+
+*/
