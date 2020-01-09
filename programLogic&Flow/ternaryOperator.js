4925498026701aac343fabe2aec2c2b41f18edc1
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2020-01-09 13:06:06 -0500

    added a ternary operator reference

---
+++ programLogic&Flow/ternaryOperator.js
@@ -0,0 +1,26 @@
+//Ternary operator
+//condition ? expIfTrue: expIfFalse
+
+let num = 7;
+
+// if (num === 7) {
+//     console.log("Lucky!");
+// } else {
+//     console.log("BAD!");
+// }
+
+//its a Ternary operator
+num == 7 ? console.log("lucky!") : console.log("BAD");
+
+
+let status = "Offline";
+
+let color;
+
+// if (status === "offline") {
+//     color = "red";
+// } else {
+//     color = "green";
+// }
+//its a Ternary operator
+let color = status === "offline" ? "red" : "green";
