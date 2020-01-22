aaaa2833cc96db92b3f96ec0db8aa8d92a2d5ef4
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2020-01-22 13:44:30 -0500

    added file modifying arrays

---
+++ arrays/modifyingArrays.js
@@ -0,0 +1,8 @@
+let colors = ['rad', 'orange', 'green', 'yellow'];
+colors[0] = "red";
+colors[2] = 'yellow';
+colors[3] = 'green';
+
+colors[4] //undefined
+colors[4] = "blue"; //undefined
+// red, orange, yellow, green, blue
