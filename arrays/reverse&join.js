c12263ebda445df96ebce3fdce0dc6bbf84f3136
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2020-02-26 14:28:15 -0500

    finished reverse&join.js examples

---
+++ arrays/reverse&join.js
@@ -0,0 +1,8 @@
+let letters = ['T', 'C', 'E', 'P', 'S', 'E', 'R'];
+
+console.log(letters.reverse()); //array is reversed
+
+
+let elements = ["fire", "water", "air"];
+
+console.log(elements.join('-'));
