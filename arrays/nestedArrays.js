3dd758c48da48d84ffe596e01a43c3899fe9df91
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2020-03-03 12:38:57 -0500

    added folder objects and file introObjects.js

---
+++ arrays/nestedArrays.js
@@ -0,0 +1,10 @@
+//Nested Arrays
+//We can store arrays inside other arrays
+const colors = [
+
+    ['blue', 'green'],
+    ['blue', 'yellow'],
+    ['orange', 'brown'],
+    ['purple', 'gold']
+
+];
---
+++ objects/intoObject.js
@@ -0,0 +1,17 @@
+//Objects
+
+/*
+ ---Objects ara collections of properties.
+ ---Properties are a key value pair.
+ ---Rather than accessing data using an index, we use custom keys.
+
+
+*/
+
+//example of an object
+let person = {
+    name: "daniel",
+    age: 29,
+    country: "canada",
+    favoriteGame: "skyrim"
+};
