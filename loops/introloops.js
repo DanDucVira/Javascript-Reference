2ac21569705635e2ba08133a7497652c9bfd824f
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2020-03-06 11:59:15 -0500

    added examples of for loops with arrays

--- loops/introloops.js
+++ loops/introloops.js
@@ -11,3 +11,25 @@
         -for in loop
 
 */
+
+
+// for loop
+for ([initialExpression];
+    [condition];
+    [incrementExpression]) {
+
+}
+// for loop example
+for (let i = 0; i <= 10; i++) {
+    console.log("welcome", i);
+
+}
+
+//for loops + arrays
+//To loop over an array,  start at 0 and continue to the last index (length -1 )
+
+const animals = ["cat", "dog", "racoon"];
+
+for (let i = 0; i < animals.length; i++) {
+    console.log(i, animals[i]);
+}
