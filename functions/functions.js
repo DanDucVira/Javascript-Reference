a3ded1e7765412cf7ef4f3d93d0e9acde5c3a945
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2020-03-16 14:33:05 -0400

    added function example

---
+++ functions/functions.js
@@ -0,0 +1,20 @@
+//Functions
+//Reusable procedures
+/*
+ --Functions allow us to write reusable, modular code.
+ --We define a "chuck" of code that we can then execute at a later point.
+ --- We use them ALL THE TIME
+
+ function has 2 step process:  Define ---> Run
+*/
+
+//example
+function funcName() {
+    //do something
+}
+
+
+//dice roll fucntion
+
+
+
