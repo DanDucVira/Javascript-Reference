0e67c0505a6f87c0a813551eeb182e9526e6fac8
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2020-05-25 13:52:02 -0400

    added file conditions.js

---
+++ conditons.js
@@ -0,0 +1,45 @@
+//Comparisons
+
+
+//== Equal to
+//5 == 5 True
+// 5 == 4 False
+
+
+//Not Equal
+//!=
+
+
+// 5 > 10 False
+//10 > 6 True
+
+// 10 >= 10 True
+//20 <= 20 TRUE
+
+//10 == "10" TRUE
+// 10 === "10" FALSE
+
+///NEVER EVER USE DOUBLE EQUALS USE THE TRIPLE (===)
+
+
+
+
+//Conditions
+
+const access = true;
+if (access) {
+    console.log("You have access to our website");
+} else {
+    console.log("You dont have access to our website");
+}
+
+const age = 20;
+
+// && and || or   != not eqaul
+if (age >= 18) {
+    console.log("You are able to connect on our website");
+
+} else {
+    console.log("WE are sorry you cannot have access to our platform");
+}
+s
