542057322363283bc7184be34eb433fd0b8975d2
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2020-05-25 12:39:32 -0400

    added file variables.js

---
+++ variables.js
@@ -0,0 +1,12 @@
+//this is a variable.
+let inbox = 100;
+
+// once declared you cannot change the value.
+const message = 2;
+
+
+
+//aways camelCase your variables.
+let yourVideos = 20;
+let userName = "Joe";
+
