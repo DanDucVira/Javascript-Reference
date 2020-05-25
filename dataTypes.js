ad95305f809b33a169f56fc9ceb3c5258dc233ad
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2020-05-25 12:57:20 -0400

    added file dataTypes.js

---
+++ dataTypes.js
@@ -0,0 +1,22 @@
+//Primitive Data types
+
+
+//Numbers
+const day = 5;
+
+//Strings
+const name = "daniel";
+
+//Booleans
+const isLogged = true;
+const isLoggedOff = false;
+
+//Undefined
+let lastName;
+
+
+//Null
+let minutes = null;
+
+//Symbol
+const num = Symbol();
