b3edc32c82a870dea4238a1dd8f1eb686691e71f
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2020-05-28 13:41:36 -0400

    added file switch.js

---
+++ switch.js
@@ -0,0 +1,19 @@
+//Switch Statements
+
+let text = "banana";
+
+switch (text) {
+    case "banana":
+        console.log("banana are great");
+        break;
+
+    case "Apple":
+        console.log("Apples are okay");
+        break;
+    case "avocado":
+        console.log("avocados are amazing");
+        break;
+    default:
+        console.log("What fruit is that");
+        break;
+}
