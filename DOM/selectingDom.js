18b9779b3e51b3c573365eb0af843eed64bcd9a0
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2020-06-04 13:10:20 -0400

    added file selectingDom.js

---
+++ DOM/selectingDom.js
@@ -0,0 +1,14 @@
+// get tags names
+let headers = document.getElementsByTagName("h2");
+
+// get class names
+let classes = document.getElementsByClassName("className");
+
+//get id
+let button = document.getElementById("idname");
+
+
+let head = document.querySelector("tagname", "classname", "idname");
+
+//returns all
+let head2 = document.querySelectorAll("all");
