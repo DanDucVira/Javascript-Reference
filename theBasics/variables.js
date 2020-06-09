ba50b3637e3fce06308f5ed925c6f33592384e19
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2020-06-09 11:46:29 -0400

    added stringsMethods.js

--- theBasics/variables.js
+++ theBasics/variables.js
@@ -1,3 +1,6 @@
+//We can store a value and give it a name. so that we can recall it, use it , or change it later on.
+
+
 //this is a variable.
 let inbox = 100;
 
@@ -9,4 +12,3 @@ const message = 2;
 //aways camelCase your variables.
 let yourVideos = 20;
 let userName = "Joe";
-
