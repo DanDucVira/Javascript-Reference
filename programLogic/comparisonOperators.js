a7911c466b73da5231d20f926f7261744f74fa27
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2020-06-16 13:28:14 -0400

    added file comparisonOperators.js

---
+++ programLogic/comparisonOperators.js
@@ -0,0 +1,13 @@
+/*
+> - greater than
+< - less than
+>= - greater than or equal to
+<= - less than or equal to
+== - equality
+!= - not equal
+=== - strict equality
+!== - strict non-equality
+
+
+
+*/
