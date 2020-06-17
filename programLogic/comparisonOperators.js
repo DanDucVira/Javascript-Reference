f22c5a15d4ade68749958870f4eb965843d8aa1e
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2020-06-17 13:52:59 -0400

    added more information in file comparisonOperators.js

--- programLogic/comparisonOperators.js
+++ programLogic/comparisonOperators.js
@@ -3,11 +3,17 @@
 < - less than
 >= - greater than or equal to
 <= - less than or equal to
-== - equality
-!= - not equal
-=== - strict equality
-!== - strict non-equality
+== - equality  --ignore them
+!= - not equal -- ignore them
+=== - strict equality --always use
+!== - strict non-equality --always use
 
 
+(==)
+* Checks for equality of value, but not equality of type.
+*It coerces both values to the same type and then compares them.
+*This can lead to some unexpercted results!
 
+(===)
+*checks for equality of value AND type
 */
