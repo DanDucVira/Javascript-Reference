1e102cba5e457ed2b4c1b38f240bd4db72824b80
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2020-06-25 13:28:01 -0400

    added more content to file arrayMethods.js

--- Arrays/arrayMethods.js
+++ Arrays/arrayMethods.js
@@ -6,6 +6,15 @@
 *Shift - removes from start
 *Unshift - add to start
 
+*concat -merge arrays
+*includes - look for a value
+*indexOf - just like str.indexOf
+*join - creates a string from arr
+*reverse - reverses an array!
+*slice  - copy portion of an arr
+*splice - removes/replace elements
+*sort - sorts an array
+
 */
 
 let nums = [1, 2, 3, 4, 5];
@@ -22,3 +31,33 @@ console.log(nums);
 
 nums.unshift(10);
 console.log(nums);
+
+
+
+//the concat() method is used to merge two or more arrays
+const array1 = [1, 2, 3];
+const array2 = [4, 5, 6];
+
+console.log(array1.concat(array2));
+
+
+// the reverse() method reverses an array in place
+let letters = ["a", "b", "c", "d", "e", "f"];
+console.log(letters.reverse());
+
+
+
+//the slice() method returns a shallow copy of a portion of an array into a new array
+let animals = ["shark", "salmon", "whale", "bear", "lizard", "tortoise"];
+
+let swimmers = animals.slice(0, 3);
+console.log(swimmers);
+
+
+//splice method changes the contents of an array by removing or replacing existing elements and/or adding newe elments in place
+animals.splice(1, 0, "octopus");
+
+
+//the sort() methods sorts the elements of an array in place and returns the sorted array.
+let people = ["Mrs.Robsinson", "Angie", "Jolene", "Maggie May", "Roxanne"];
+people.sort();
