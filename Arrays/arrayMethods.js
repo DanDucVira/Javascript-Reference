584950023c386f6d97aa5340246a6dbd0b2e9608
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2020-06-25 12:19:30 -0400

    added file arrayMethods.js

---
+++ Arrays/arrayMethods.js
@@ -0,0 +1,24 @@
+/* Array Methods
+
+
+*Push - add to end
+*Pop - remove from end
+*Shift - removes from start
+*Unshift - add to start
+
+*/
+
+let nums = [1, 2, 3, 4, 5];
+
+nums.push(6);
+console.log(nums);
+
+nums.pop();
+console.log(nums);
+
+
+nums.shift();
+console.log(nums);
+
+nums.unshift(10);
+console.log(nums);
