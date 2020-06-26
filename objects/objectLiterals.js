49e0735f6b915801139e41ed236d3d547f575447
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2020-06-26 12:51:20 -0400

    added file objectLiterals.js

---
+++ objects/objectLiterals.js
@@ -0,0 +1,10 @@
+/*Object Literals */
+const comment = {
+    username: "sillyGoose219",
+    downVotes: 19,
+    upVotes: 216,
+    netScore: 195,
+    commentTExt: "tastes like chicken lol",
+    tags: ["#hilarious", "funny", "silly"],
+    isGilded: false
+};
