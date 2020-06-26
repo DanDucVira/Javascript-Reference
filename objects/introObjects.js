31e0b12359784e7eca41367b053a79ed0eeab9f8
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2020-06-26 12:41:54 -0400

    added file introObjects.js

---
+++ objects/introObjects.js
@@ -0,0 +1,13 @@
+/*Objects
+ *Objects are collections of properties/
+ *Properties are a key value pair
+ *Rather than accessing data using an index, we use custom keys.
+ */
+
+const fitbitData = {
+    totalSteps: 30000,
+    totalMiles: 200.4,
+    avgCalorieBurn: 5768,
+    workOutsThisWeek: "5 of 7",
+    avgGoodSleep: "2:13"
+};
