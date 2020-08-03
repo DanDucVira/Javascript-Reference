744f087111248b3f957bd0920c9f28e5215ab037
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2020-08-03 02:32:13 -0400

    added folder Functions and file functions.js

---
+++ functions/functions.js
@@ -0,0 +1,11 @@
+//Functions
+/*
+* functions allow us to write reusable, modular code
+*we define a "chunk" of code that we can then execute at a later point.
+*we use them all the time.
+
+*/
+
+function funcName(){
+    //do something
+}
