91c52c009128b588ee5ea22a3598d95e7fa1c647
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2020-08-03 12:21:58 -0400

    added file functions.js

--- functions/functions.js
+++ functions/functions.js
@@ -6,6 +6,57 @@
 
 */
 
-function funcName(){
+function funcName() {
     //do something
+}
+
+//example
+function rollDie() {
+    let roll = Math.floor(Math.random() * 6) + 1;
+    console.log(`Rolled: ${roll}`);
+
+}
+rollDie();
+
+
+
+//function arguments
+
+function greet(name) {
+    console.log(`Hello, ${name}!`);
+}
+greet("Daniel");
+
+
+//functions with multiple arguments
+
+function sum(x, y) {
+    console.log(x + y);
+}
+sum(4, 6);
+
+
+function divide(a, b) {
+    console.log(a / b);
+
+}
+divide(10, 5);
+
+//return statement
+/*
+Build-in methods return values when we call them.
+We can capture a return value in a variable.
+The return statement ends function execution and specifies the value to be returned by the function.
+ */
+
+function add(x, y) {
+    return x + y;
+
+}
+const sum = add(2, 2);
+sum; //4
+
+
+function isGreen(color) {
+    return color.toLowerCase() === "green";
 }
