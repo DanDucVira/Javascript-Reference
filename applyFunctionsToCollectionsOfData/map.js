8d607c412c146e9bc96222bd4708e3664041650e
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2020-09-20 22:49:01 -0400

    added file map.js

---
+++ applyFunctionsToCollectionsOfData/map.js
@@ -0,0 +1,14 @@
+//MAP
+/*
+Creates a new array with the results of calling a callback on every element in the array
+ */
+
+
+
+const texts = ["rofl", "lol", "omg", "ttyl"];
+const caps = text.map(function (t) {
+    return t.toUpperCase();
+})
+
+//texts; //["rofl", "lol", "omg", "ttyl"]
+//caps; ["ROFL", "LOL", "OMG", "TTYL"]
