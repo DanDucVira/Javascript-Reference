93e8e278ce375fafa2feff36f86d450c0ee1d0e8
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2020-09-20 22:30:01 -0400

    added example in file

--- applyFunctionsToCollectionsOfData/forEach.js
+++ applyFunctionsToCollectionsOfData/forEach.js
@@ -1,7 +1,13 @@
-//FOREACH -> Accepts a callback function. Calls the unction once per element in the array.
+//FOREACH -> Accepts a callback function. Calls the function once per element in the array.
 const numbers = [20, 21, 22, 23, 24, 25, 26, 27];
 
 
 numbers.forEach(function (num) {
     console.log(num * 2);
 });
+
+function printTriple(n) {
+    console.log(n * 3);
+}
+
+numbers.forEach(printTriple);
