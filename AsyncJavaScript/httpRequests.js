93c2b9a757aa831b9cbb9ff8842e63d0955cd16c
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2020-09-29 07:42:06 -0400

    added file httprequests.js

---
+++ AsyncJavaScript/httpRequests.js
@@ -0,0 +1,23 @@
+/*
+HTTP Requests
+
+
+*Make HTTP requests to get data from another server
+-We make these requests to API endpoints
+
+*/
+
+
+const request = new XMLHttpRequest();
+
+
+request.addEventListener("readystatechange", () => {
+    // console.log(request, request.readyState);
+    if (request.readyState === 4) {
+        console.log(request.responseText);
+    }
+});
+request.open("GET", "https://jsonplaceholder.typicode.com/todos/");
+request.send();
+
+
